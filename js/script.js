1f2c962def89efbba6ced01706055c6baade3435
Ritana . <ksenja.vassiljeva@ivkhk.ee>
2026-05-12 00:46:29 +0300

    added button like and dislikes

--- js/script.js
+++ js/script.js
@@ -62,3 +62,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
+const likeButtons = document.querySelectorAll(".like-btn");
+
+likeButtons.forEach(button => {
+  button.addEventListener("click", () => {
+
+    const isLoggedIn = localStorage.getItem("user");
+
+    if (!isLoggedIn) {
+      window.location.href = "login.html";
+      return;
+    }
+
+    alert("Лайк поставлен!");
+  });
+});
